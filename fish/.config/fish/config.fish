2d25ccfd836c928655bfdaf54346f663c7f98b68
amosquet <artusmosquet@gmail.com>
2026-01-28 22:21:25 -0500

    base files

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+	alias ls='ls --color=auto'
+	alias grep='grep --color=auto'
+
+	set PATH $PATH ~/.cargo/bin
+
+	if status is-interactive and not set -q TMUX and not set -q SSH_CONNECTION
+		exec tmux new-session -A -s main
+	end
+end
